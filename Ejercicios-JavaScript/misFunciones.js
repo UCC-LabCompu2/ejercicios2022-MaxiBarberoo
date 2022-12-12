cc98af688304996da491e0404f5345db20356dbf
MaxiBarbero <mbarbero48@gmail.com>
2022-12-12 10:59:24 -0300

    Ejercicios Terminados

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,16 +6,28 @@
  */
 
 function conversionUnidades(id, valor){
-    if(isNan(valor)){
+    if(isNaN(valor)){
         alert("Se ingreso un valor incorrecto en " + id);
         document.lasUnidades.unid_metro.value = "";
         document.lasUnidades.unid_pulgada.value = "";
         document.lasUnidades.unid_pie.value = "";
         document.lasUnidades.unid_yarda.value = "";
-    }else if(id == "metro"){
+    }else if(id === "metro"){
         document.lasUnidades.unid_pulgada.value = 39.3701*valor;
         document.lasUnidades.unid_pie.value = 3.28084*valor;
         document.lasUnidades.unid_yarda.value = 1.09361*valor;
+    }else if(id === "yarda"){
+        document.lasUnidades.unid_metro.value = 0.9144*valor;
+        document.lasUnidades.unid_pulgada.value = 36*valor;
+        document.lasUnidades.unid_pie.value = 3*valor;
+    }else if(id === "pulgada"){
+        document.lasUnidades.unid_metro.value = 0.0254*valor;
+        document.lasUnidades.unid_pie.value = 0.08333*valor;
+        document.lasUnidades.unid_yarda.value = 0.027778*valor;
+    }else if(id === "pie"){
+        document.lasUnidades.unid_metro.value = 0.3048*valor;
+        document.lasUnidades.unid_pulgada.value = 12*valor;
+        document.lasUnidades.unid_yarda.value = 0.3333*valor;
     }
 }
 
